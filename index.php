42089cba993f3bc5ddada1b3f97149143724b22b
Handi Hermawan <95008722+handimanny@users.noreply.github.com>
2022-04-19 08:58:54 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header("location: https://www.canva.com/design/DAE-PMJeMzs/F1I9_HrN-yRz67JI6l8clQ/view?utm_content=DAE-PMJeMzs&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton");
+?>
