432c0028d55d7b034905b32d3eb465f85ce17567
handimanny <handimanny10@gmail.com>
2023-02-23 09:51:05 +0700

    update beautify

--- desa/script.js
+++ desa/script.js
@@ -1,16 +1,16 @@
-(function() {
+(function () {
 
     var preload = document.getElementById("preload");
     var loading = 0;
     var id = setInterval(frame, 64);
 
     function frame() {
-        if(loading == 100) {
+        if (loading == 100) {
             clearInterval(id);
             window.open("index/index.html", "_self");
         } else {
             loading = loading + 1;
-            if(loading == 90) {
+            if (loading == 90) {
                 preload.style.animation = "fadeout ls ease"
             }
         }
